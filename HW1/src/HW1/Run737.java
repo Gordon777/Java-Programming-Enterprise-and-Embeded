af072ee1fb718c71de3308c33ed5cdf643263499
Gordon777 <goydon6a@gmail.com>
2019-06-29 22:55:26 +0800

    new

---
+++ HW1/src/HW1/Run737.java
@@ -0,0 +1,11 @@
+package HW1;
+
+public class Run737 {
+	  public static void main(String[] args)
+	  {
+	    SimpletronMachine stMachine = new SimpletronMachine();
+	    stMachine.inputProgram();
+	    stMachine.executeProgram();
+	  }
+
+}
